0e9928f5ac206ab6363e04e49a033148c6014dae
LePtiDev <75quentinguerrier@gmail.com>
2020-10-12 22:16:52 +0200

    [add] bootstrap and footer and header

--- app/views/error.php
+++ app/views/error.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="<?php __DIR__ ?>public/css/app.css">
+    <?php require_once(__DIR__ . '/partials/header.php') ?>
     <title>Erreur 404</title>
 </head>
 <body>
---
+++ app/views/user.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php require_once(__DIR__ . '/partials/header.php') ?>
+    <title>Utilisateur</title>
+</head>
+<body>
+    <div class="container-fluid pt-3">
+        <div class="row d-flex justify-content-center mb-5">
+            <h2>Page user</h2>
+        </div>
+        <div class="row pr-5 d-flex justify-content-end">
+            <button class="btn btn-success">Ajouter un utilisateur</button>
+        </div>
+    </div>
+
+    <?php require_once(__DIR__ . '/partials/footer.php') ?>
+</body>
+</html>
